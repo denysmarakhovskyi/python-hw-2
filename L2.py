9e8e61be6a829a43241792b6aa706a91b8d50eb3
denysmarakhovskyi <denysmarakhovskyi@gmail.com>
2022-07-18 00:28:15 +0200

    L2

--- L2.py
+++ L2.py
@@ -3,35 +3,28 @@ num = int(input())
 
 def oddOrEven(num):
     if num % 2 == 0:
-        print('Число Х є парним')
+        print('Число є парним')
     else:
-        print('Число Х є непарним')
-    return 0
+        print('Число є непарним')
 
 
 print(oddOrEven(num))
 
-
 print('\n')
 
+str = str(input('Enter string: '))
 
-str = 'Testing string', 'Lux', 'up', 'Luxery'
 
-
-def transformStr(str):
-    if str[0] == 'L' and str[0] == 'l':
+def string(str):
+    fiveSymbols = (str[0:5] + "...")
+    if len(str) > 5:
+        print(fiveSymbols)
+    elif str.startswith('u') or 'U':
+        print(str.upper())
+    elif str.startswith('l') or 'L':
+        print(str.lower())
+    else:
         print(str)
-    elif str[0] == 'U' and str[0] == 'u':
-        print(str)
-    elif len(str) >= 5:
-        print(str+'...')
 
 
-print(transformStr(str))
-
-
-
-transformStr('Testing string')  # 'Testi...' (довжина більше 5 символів)
-transformStr('Lux')  # 'lux' (Починается на L)
-transformStr('up')  # 'UP' (Починается на U)
-transformStr('Luxery')  # 'luxer...' (Починается на L + довжина більше 5 символів)
+print(string(str))
